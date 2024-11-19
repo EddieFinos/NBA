e2d6b26b742c2ad57103282fdc430d583ffc171f
EddieFinos <edwardfinos@yahoo.com>
2024-11-19 15:11:18 -0500

    Update Test.py
    Made functions for finding:
    player id
    players gamelog
    players average points (season)
    players average points (all time)

--- Test.py
+++ Test.py
@@ -8,32 +8,53 @@ Created on Thu Nov 14 20:58:21 2024
 import pandas as pd
 
 from nba_api.stats.static import players
-player_dict = players.get_players()
-
 from nba_api.stats.endpoints import playergamelog
-import pandas as pd 
-
 from nba_api.stats.library.parameters import SeasonAll
 
+player_dict = players.get_players()
 
 def player_id(player_name):
     for player in player_dict:
         if player['full_name'] == player_name:
             return player['id']
 
-print(player_id('JamesOn Curry'))
-
-
-
-
 def player_gamelog(player_id):
     player_gamelog = playergamelog.PlayerGameLog(player_id= player_id, season = SeasonAll.all)
     df_player_gamelog = player_gamelog.get_data_frames()[0]
     return df_player_gamelog
-df = player_gamelog(201191)
+
+
+def average_points_all(gamelog):
+    total_points = 0
+    total_games = 0
+    for _, game in gamelog.iterrows():
+        if game['MIN'] > 0:
+            total_games += 1
+            total_points += game['PTS']
+    average = total_points / total_games if total_games > 0 else 0
+    return average
+
+def average_points_season(gamelog, season):
+    total_points = 0
+    total_games = 0
+    for _, game in gamelog.iterrows():
+        if game['MIN'] > 0 and game['SEASON_ID'].endswith(season):
+            total_games += 1
+            total_points += game['PTS']
+    average = total_points / total_games if total_games > 0 else 0
+    return average
+
+
+player = player_id('Stephen Curry')
+player_gamelog = player_gamelog(player)
+average_all = average_points_all(player_gamelog)
+average_season = average_points_season(player_gamelog, '22024')
 
 
 
 
-print(player_id('JamesOn Curry'))
-df = player_gamelog(201191)
+
+
+
+
+
